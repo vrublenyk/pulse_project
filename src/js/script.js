44f88c5fc6ab3f97477de5b6397fcc4278f50b39
volodymyr rublenyk home pc <vrublenyk94@gmail.com>
2023-01-12 21:43:25 +0100

    commit 12.01

--- src/js/script.js
+++ src/js/script.js
@@ -48,5 +48,70 @@ $(document).ready(function(){
     };
     toggleSlide('.catalog-item__link')
     toggleSlide('.catalog-item__back')
+
+    //modal
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+
+    });
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+    });
+    $('.button_mini').each(function(i){
+      $(this).on('click',function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      });
+    });
+    function validateForms(form){
+      $(form).validate({
+        rules:{
+          name:{
+            required: true,
+            minlength: 2
+          },
+          phone:"required",
+          email:{
+            required:true,
+            email:true
+          }
+        },
+        messages:{
+          name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символа!")
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "неправильно введен адрес почы"
+          }
+        }
+      });
+    };
     
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    //smooth scroll pageup
+
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 1000){
+        $('.pageup').fadeIn();
+      } else{
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $("a[href^='#']").click(function(){
+      var _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+    
+    new WOW().init();
   });
